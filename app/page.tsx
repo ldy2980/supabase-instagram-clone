1022a5d00704ed2aa9016d0fcc2c063f40d655d8
ldy2980 <ldyclank@gmail.com>
2024-10-15 21:48:07 +0900

    UI 구성

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import LogoutButton from "components/logout-button";
+
 export const metadata = {
   title: "InstaClone",
   description: "Instagram clone",
 };
 
 export default function Home() {
-  return <main className="">InstaClone</main>;
+  return (
+    <main className="w-full h-screen flex flex-col gap-2 items-center justify-center">
+      <h1 className="font-bold text-xl">Welcome {"USER"}</h1>
+      <LogoutButton />
+    </main>
+  );
 }
